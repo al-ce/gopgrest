217dacc2bdb6ee46ab92c9c37d4764a168ff18f7
al-ce <23170004+al-ce@users.noreply.github.com>
2025-07-11 18:09:44 -0500

    feat(app): connect to database and run api server

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module ftrack
 
 go 1.24.5
+
+require github.com/lib/pq v1.10.9
--- main.go
+++ main.go
@@ -1,31 +1,76 @@
 package main
 
 import (
+	"database/sql"
+	"fmt"
 	"log"
 	"net/http"
+	"os"
+	"time"
+
+	_ "github.com/lib/pq"
 )
 
-// Set represents a performed set
-type Set struct {
-	Name    string  `json:"name"`
-	Date    string  `json:"date"`
-	Output  float32 `json:"output"`
-	Unit    string  `json:"unit"`
-	Reps    int8    `json:"reps"`
-	Set     int8    `json:"set"`
-	Notes   string  `json:"notes"`
-	Day     string  `json:"day"`
-	Program string  `json:"program"`
-	Tags    string  `json:"tags"`
-}
+const (
+	host = "localhost"
+	port = 5432
+	user = "postgres"
+)
 
-func main() {
+var (
+	dbname = os.Getenv("DB_NAME")
+	dbpass = os.Getenv("DB_PASS")
+)
+
+func run() error {
+	// Define connection params
+	dbParams := fmt.Sprintf(
+		"host=%s port=%d user=%s password=%s dbname=%s sslmode=disable",
+		host, port, user, dbpass, dbname,
+	)
+
+	// Open db connection
+	db, err := sql.Open("postgres", dbParams)
+	if err != nil {
+		panic(err)
+	}
+	defer db.Close()
+
+	// Establish connection
+	err = db.Ping()
+	if err != nil {
+		log.Println(err)
+	}
+
+	APIHandler := NewAPIHandler(db)
+
 	// Create server and routes
 	mux := http.NewServeMux()
-	mux.Handle("/", &HistoryHandler{})
+	mux.Handle("/", &APIHandler)
 
 	// Run server
 	log.Println("Listening on port 8090...")
-	err := http.ListenAndServe(":8090", mux)
+	err = http.ListenAndServe(":8090", mux)
 	log.Fatal(err)
+
+	return nil
+}
+
+func main() {
+	if err := run(); err != nil {
+		log.Fatal(err)
+	}
+}
+
+type ExerciseSet struct {
+	Name        string
+	PerformedAt time.Time
+	Weight      float32
+	Unit        string
+	Reps        int
+	SetCount    int
+	Notes       string
+	SplitDay    string
+	Program     string
+	Tags        string
 }
