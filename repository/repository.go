ee387d09ae330d9a6ee1ddd293971f0e256c032c
al-ce <23170004+al-ce@users.noreply.github.com>
2025-07-11 22:32:43 -0500

    feat(repo): add ListSets method to Repository interface

--- repository/repository.go
+++ repository/repository.go
@@ -15,3 +15,13 @@ func NewRepository(db *sql.DB) Repository {
 		db: db,
 	}
 }
+
+// ListSets retrieves the list of all sets from the exercise_sets table
+func (r *Repository) ListSets() (*sql.Rows, error) {
+	const listStmt = "select * from exercise_sets"
+	rows, err := r.db.Query(listStmt)
+	if err != nil {
+		return nil, err
+	}
+	return rows, nil
+}
