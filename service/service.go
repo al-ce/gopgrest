2b09db4a97ab30c42a9c99d3318512b21b53f66b
al-ce <23170004+al-ce@users.noreply.github.com>
2025-07-11 22:34:24 -0500

    feat(service): add ListSet to Service interface

--- service/service.go
+++ service/service.go
@@ -1,6 +1,7 @@
 package service
 
 import (
+	"database/sql"
 	"time"
 
 	"ftrack/models"
@@ -29,4 +30,46 @@ func (s *Service) CreateSet(setData models.ExerciseSet) error {
 	setData.PerformedAt = setData.PerformedAt.Round(time.Second)
 
 	return s.repo.CreateSet(&setData)
+}
+
+// ListSets retrieves the list of all sets from the exercise_sets table
+func (s *Service) ListSets() ([]models.ExerciseSet, error) {
+	rows, err := s.repo.ListSets()
+	if err != nil {
+		return []models.ExerciseSet{}, err
+	}
+	defer rows.Close()
+
+	// Scan rows into struct slice
+	sets, err := scanExerciseSetRows(rows)
+	if err != nil {
+		return []models.ExerciseSet{}, err
+	}
+	return sets, nil
+}
+
+// scanExerciseSetRows scans sql rows into a slice of ExerciseSet structs
+func scanExerciseSetRows(rows *sql.Rows) ([]models.ExerciseSet, error) {
+	sets := []models.ExerciseSet{}
+	for rows.Next() {
+		set := &models.ExerciseSet{}
+		err := rows.Scan(
+			&set.ID,
+			&set.Name,
+			&set.PerformedAt,
+			&set.Weight,
+			&set.Unit,
+			&set.Reps,
+			&set.SetCount,
+			&set.Notes,
+			&set.SplitDay,
+			&set.Program,
+			&set.Tags,
+		)
+		if err != nil {
+			return sets, err
+		}
+		sets = append(sets, *set)
+	}
+	return sets, nil
 }
