c828627d8b48ff218fc5f280158ff652387367da
al-ce <23170004+al-ce@users.noreply.github.com>
2025-07-13 11:27:37 -0500

    feat(repo/list): add WHERE clause from map of params

--- repository/repository.go
+++ repository/repository.go
@@ -20,9 +20,13 @@ func NewRepository(db *sql.DB) Repository {
 }
 
 // ListSets retrieves the list of all sets from the exercise_sets table
-func (r *Repository) ListSets() (*sql.Rows, error) {
-	const listStmt = "select * from exercise_sets"
-	rows, err := r.db.Query(listStmt)
+func (r *Repository) ListSets(params map[string][]string) (*sql.Rows, error) {
+	// Build list query with optional conditional filters
+	conditional, values := buildConditionalClause(params)
+	listStmt := "select * from exercise_sets" + conditional
+
+	// Execute list query
+	rows, err := r.db.Query(listStmt, values...)
 	if err != nil {
 		return nil, err
 	}
@@ -98,4 +102,36 @@ func (r *Repository) DeleteSet(id string) error {
 		return err
 	}
 	return nil
+}
+
+// buildConditionalClause builds a SQL WHERE clause to select a row. Ex: when
+// params == `[name:[bob alice] age:[45]]`, the name in the row must be either
+// bob or alice and the age must be 45.
+func buildConditionalClause(params map[string][]string) (string, []any) {
+	// If no params were passed, there should not be a WHERE clause
+	if len(params) == 0 {
+		return "", []any{}
+	}
+
+	clause := " WHERE ("
+	values := []any{}
+
+	// n is the number of the placeholder in the statement e.g. $1
+	n := 1
+	for k, vals := range params {
+		// Join all values for this key with OR, allow any match
+		for _, v := range vals {
+			clause += fmt.Sprintf("%s = $%d OR ", k, n)
+			n += 1
+			values = append(values, v)
+		}
+		// Strip final " OR "
+		clause = clause[:len(clause)-4]
+		// Join all keys with AND, require at least one match from each key
+		clause += ") AND ("
+	}
+	// Strip final " AND ("
+	clause = clause[:len(clause)-6]
+
+	return clause, values
 }
