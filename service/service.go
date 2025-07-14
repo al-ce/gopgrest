c939451d598edb01a02155c5a7c32d7bd8ad0759
al-ce <23170004+al-ce@users.noreply.github.com>
2025-07-14 01:24:58 -0500

    feat(service): scan query results into a map with any value; specify table for query

--- service/service.go
+++ service/service.go
@@ -4,8 +4,8 @@ import (
 	"database/sql"
 	"encoding/json"
 	"fmt"
+	"reflect"
 
-	"ftrack/models"
 	"ftrack/repository"
 )
 
@@ -26,72 +26,89 @@ func (s *Service) InsertRow(newRow *map[string]any, table string) error {
 	if !s.repo.TableExists(table) {
 		return fmt.Errorf("table %s does not exist", table)
 	}
-	return s.repo.InsertRow(newRow, table)
+	return s.repo.InsertRow(table, newRow)
 }
 
-// ListSets retrieves the list of all sets from the exercise_sets table
-func (s *Service) ListSets(params map[string][]string) ([]models.ExerciseSet, error) {
-	rows, err := s.repo.ListSets(params)
+// ListRows gets rows from a table with optional filter params
+func (s *Service) ListRows(table string, params map[string][]string) ([]map[string]any, error) {
+	rows, err := s.repo.ListRows(table, params)
 	if err != nil {
-		return []models.ExerciseSet{}, err
+		return []map[string]any{}, err
 	}
 	defer rows.Close()
 
 	// Scan rows into struct slice
-	sets, err := scanExerciseSetRows(rows)
+	listQueryResults, err := scanRows(rows)
 	if err != nil {
-		return []models.ExerciseSet{}, err
+		return []map[string]any{}, err
 	}
-	return sets, nil
+	return listQueryResults, nil
 }
 
-// UpdateSet updates any number of valid ExerciseSet fields with separate calls
-// to Repository.UpdateSetField
-func (s *Service) UpdateSet(id string, updateData map[string]any) error {
-	// Decode request body into a dummy ExerciseSet value to validate fields
-	var dummyExerciseSet models.ExerciseSet
+// UpdateRow updates any number of valid fields with separate calls to
+// Repository.UpdateRowCol
+func (s *Service) UpdateRow(table, id string, updateData map[string]any) error {
+	// Decode request body into a dummy row value to validate fields
+	var dummyRow map[string]any
 	b, _ := json.Marshal(updateData)
-	err := json.Unmarshal(b, &dummyExerciseSet)
+	err := json.Unmarshal(b, &dummyRow)
 	if err != nil {
 		return err
 	}
 
-	// Call Repository.UpdateSetField for each field to update
+	// Update individual columns in the row
 	for field, val := range updateData {
-		if err := s.repo.UpdateSetField(id, field, val); err != nil {
+		if err := s.repo.UpdateRowCol(table, id, field, val); err != nil {
 			return err
 		}
 	}
 	return nil
 }
 
-// DeleteSet removes a set from the exercise_sets table by id
-func (s *Service) DeleteSet(id string) error {
-	return s.repo.DeleteSet(id)
+// DeleteRow removes a row form the table by id
+func (s *Service) DeleteRow(table, id string) error {
+	return s.repo.DeleteRow(table, id)
 }
 
-// scanExerciseSetRows scans sql rows into a slice of ExerciseSet structs
-func scanExerciseSetRows(rows *sql.Rows) ([]models.ExerciseSet, error) {
-	sets := []models.ExerciseSet{}
-	for rows.Next() {
-		set := &models.ExerciseSet{}
-		err := rows.Scan(
-			&set.ID,
-			&set.Name,
-			&set.PerformedAt,
-			&set.Weight,
-			&set.Unit,
-			&set.Reps,
-			&set.SetCount,
-			&set.Notes,
-			&set.SplitDay,
-			&set.Program,
-			&set.Tags,
-		)
-		if err != nil {
-			return sets, err
-		}
-		sets = append(sets, *set)
+// scanRows scans rows from a query into a map
+func scanRows(rows *sql.Rows) ([]map[string]any, error) {
+	// Get column names
+	cols, err := rows.Columns()
+	if err != nil {
+		return []map[string]any{}, err
 	}
-	return sets, nil
+
+	// Get column types
+	coltypes, err := rows.ColumnTypes()
+	if err != nil {
+		return []map[string]any{}, err
+	}
+
+	// Create slice to hold pointers of type/size equivalent to column type
+	rowValues := make([]any, len(cols))
+	rowPtrs := make([]any, len(cols))
+	for i := range rowValues {
+		rowValues[i] = reflect.Zero(coltypes[i].ScanType())
+		rowPtrs[i] = &rowValues[i]
+	}
+
+	listQueryResults := []map[string]any{}
+
+	// Scan column values into pointer slice
+	for rows.Next() {
+		err := rows.Scan(rowPtrs...)
+		if err != nil {
+			return []map[string]any{}, err
+		}
+		// Create map of column names and column values
+		scannedRow := map[string]any{}
+		for i := range len(cols) {
+			col := cols[i]
+			scannedRow[col] = rowValues[i]
+
+		}
+		listQueryResults = append(listQueryResults, scannedRow)
+	}
+
+	return listQueryResults, nil
 }
