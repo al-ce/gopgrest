727b511afb2e9edb7aad24893dc4e7f14f712ca0
al-ce <23170004+al-ce@users.noreply.github.com>
2025-07-27 00:49:20 -0500

    test(queries): test more insert cases

--- repository/insert_test.go
+++ repository/insert_test.go
@@ -1,6 +1,7 @@
 package repository_test
 
 import (
+	"fmt"
 	"testing"
 
 	_ "github.com/lib/pq"
@@ -14,16 +15,53 @@ func TestInsertRow(t *testing.T) {
 
 	repo := repository.NewRepository(tdb.DB)
 
-	t.Run("ins row into valid table", func(t *testing.T) {
-		newRow := map[string]any{
-			"name":   "deadlift",
-			"weight": 200,
-			"ieninreps":   10,
-		}
+	insertTests := []struct {
+		name   string
+		newRow map[string]any
+		expect any
+	}{
+		{
+			"ins row with valid col names/values",
+			map[string]any{
+				"name":   "deadlift",
+				"weight": 200,
+				"reps":   10,
+			},
+			nil,
+		},
+		{
+			"ins row with missing req cols",
+			map[string]any{
+				"weight": 200,
+				"reps":   10,
+			},
+			"pq: null value in column \"name\" of relation \"exercise_sets\" violates not-null constraint",
+		},
+		{
+			"ins row with invalid values",
+			map[string]any{
+				"weight": "not int",
+			},
+			"pq: invalid input syntax for type smallint: \"not int\"",
+		},
+		{
+			"ins with invalid col names",
+			map[string]any{
+				"not_a_col": 10,
+			},
+			fmt.Sprintf(
+				"pq: column \"not_a_col\" of relation \"%s\" does not exist",
+				tests.TABLE1),
+		},
+	}
 
-		err := repo.InsertRow(tests.TABLE1, &newRow)
-		if err != nil {
-			t.Errorf("Err: %v", err)
-		}
-	})
+	for _, tt := range insertTests {
+		t.Run(tt.name, func(t *testing.T) {
+			err := repo.InsertRow(tests.TABLE1, &tt.newRow)
+			if (tt.expect == nil && err != nil) ||
+				(err != nil && err.Error() != tt.expect) {
+				t.Errorf("Expected error: %v\nGot %v", tt.expect, err)
+			}
+		})
+	}
 }
