9e894d631957b19760b00552f30988b03ff5445b
al-ce <23170004+al-ce@users.noreply.github.com>
2025-07-27 21:23:45 -0500

    test(repo): test update query

---
+++ repository/update_test.go
@@ -0,0 +1,70 @@
+package repository_test
+
+import (
+	"fmt"
+	"reflect"
+	"testing"
+
+	"ftrack/repository"
+	"ftrack/tests"
+	"ftrack/types"
+)
+
+type updateTest struct {
+	testName  string
+	id        string
+	field     string
+	value     string
+	expectErr any
+}
+
+func TestUpdateRowCol(t *testing.T) {
+	tdb := tests.GetTestDB(t)
+
+	tx := tdb.BeginTX(t)
+	repo := repository.NewRepository(tx)
+
+	sampleRow := types.RowDataMap{
+		"Name":   "romanian deadlift",
+		"Weight": 309,
+	}
+
+	insertResult := repo.InsertRow(tests.TABLE1, &sampleRow)
+	if insertResult.Error != nil {
+		t.Errorf("Insert err %s", insertResult.Error)
+	}
+
+	updateTests := []updateTest{
+		{
+			"update valid field",
+			fmt.Sprintf("%d", insertResult.ID),
+			"Name",
+			"hack squat",
+			nil,
+		},
+	}
+
+	for _, tt := range updateTests {
+		// Exec update query
+		err := repo.UpdateRowCol(tests.TABLE1, tt.id, tt.field, tt.value)
+		if err != tt.expectErr {
+			t.Errorf("Expected err: %s\nGot %s", tt.expectErr, err)
+		}
+		// Get updated row
+		updatedRow := tests.ExerciseSet{}
+		err = tests.ScanExerciseSetRow(
+			&updatedRow,
+			repo.GetRowByID(tests.TABLE1, int(insertResult.ID)),
+		)
+		if err != nil {
+			t.Errorf("Scan err: %s", err)
+		}
+		// Confirm update
+		rowVal := reflect.ValueOf(updatedRow)
+		gotVal := fmt.Sprintf("%v", rowVal.FieldByName(tt.field))
+		if tt.value != gotVal {
+			t.Errorf("Expected %s: %s\nGot %s", tt.field, tt.value, gotVal)
+		}
+
+	}
+}
