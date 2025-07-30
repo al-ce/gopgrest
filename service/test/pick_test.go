2e9dedb60db6def3a08cab9c3da6da42e1d00939
al-ce <23170004+al-ce@users.noreply.github.com>
2025-07-29 22:13:26 -0500

    test(service): fix test pick row with valid ids

--- service/test/pick_test.go
+++ service/test/pick_test.go
@@ -10,6 +10,8 @@ import (
 func Test_PickRow(t *testing.T) {
 	serv, sampleRows := tests.NewTestService(t)
 
+	tagMap := tests.GetTagMap(tests.ExerciseSet{})
+
 	t.Run("pick with valid ids", func(t *testing.T) {
 		for id, sample := range sampleRows {
 			rowDataMap, err := serv.PickRow(tests.TABLE1, fmt.Sprintf("%d", id))
@@ -17,8 +19,10 @@ func Test_PickRow(t *testing.T) {
 				t.Errorf("pick err: %s", err)
 			}
 			for k, v := range sample {
-				if v != rowDataMap[k] {
-					t.Errorf("Expected %s: %v\nGot: %v", k, v, rowDataMap[k])
+				colName := tagMap[k]
+				colVal := rowDataMap[colName]
+				if fmt.Sprintf("%v", v) != fmt.Sprintf("%v", colVal) {
+					t.Errorf("Expected %s: %v %T\nGot: %v %T", k, v, v, colVal, colVal)
 				}
 			}
 		}
