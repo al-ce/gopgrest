892ff1a2547545777876f90de0dec8de49e03c96
al-ce <23170004+al-ce@users.noreply.github.com>
2025-07-30 19:17:25 -0500

    test(service): update InsertRows tests to use CustomErr

--- service/test/insert_test.go
+++ service/test/insert_test.go
@@ -12,8 +12,8 @@ func TestService_InsertRows(t *testing.T) {
 		t.Run(tt.Name, func(t *testing.T) {
 			serv, _ := tests.NewTestService(t)
 			_, err := serv.InsertRow(&tt.NewRow, tests.TABLE1)
-			if fmt.Sprintf("%s", tt.PqErr) != fmt.Sprintf("%s", err) {
-				t.Errorf("Got: %v", err)
+			if fmt.Sprintf("%s", tt.CustomErr) != fmt.Sprintf("%s", err) {
+				t.Errorf("\nExp: %v\nGot: %v", tt.CustomErr, err)
 			}
 		})
 	}
