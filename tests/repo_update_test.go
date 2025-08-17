ade04ee68043969edebaae7876aed763c982b093
al-ce <23170004+al-ce@users.noreply.github.com>
2025-08-17 10:48:39 -0500

    test(repo/update): can update rows by rsql

--- tests/repo_update_test.go
+++ tests/repo_update_test.go
@@ -3,6 +3,7 @@ package tests
 import (
 	"testing"
 
+	"gopgrest/rsql"
 	"gopgrest/service"
 	"gopgrest/types"
 )
@@ -36,6 +37,40 @@ func Test_RepoUpdateRowById(t *testing.T) {
 	if err != nil {
 		t.Fatalf("Could not scan author rows: %s", err)
 	}
+	if err := checkMapEquality(expAuthors, gotRows); err != nil {
+		t.Errorf("%s\nExp %v\nGot %v", err, expAuthors, gotRows)
+	}
+}
+
+func Test_RepoUpdateRowByRSQL(t *testing.T) {
+	repo := NewTestRepo(t)
+	expAuthors := []types.RowData{AnneCarson, AnneBrontë}
+	// Set forename column in expected author values
+	for _, expAuth := range expAuthors {
+		expAuth["forename"] = "Beatrice"
+	}
+
+	// Update forename for each author named 'Anne'
+	update := types.RowData{"forename": "Beatrice"}
+	filters := []rsql.Filter{
+		{Column: "forename", Values: []string{"Anne"}, SQLOperator: "="},
+	}
+	err := repo.UpdateRowByRSQL("authors", filters, &update)
+	if err != nil {
+		t.Errorf("Update by RSQL err: %s", err)
+	}
+
+	// Get all authors initially named 'Anne', i.e. all but Virginia
+	rows, err := repo.DB.Query("SELECT * FROM authors WHERE forename != 'Virginia' ORDER BY id")
+	if err != nil {
+		t.Fatal(err)
+	}
+	defer rows.Close()
+	gotRows, err := service.ScanRows(rows)
+	if err != nil {
+		t.Fatal(err)
+	}
+
 	if err := checkMapEquality(expAuthors, gotRows); err != nil {
 		t.Errorf("%s\nExp %v\nGot %v", err, expAuthors, gotRows)
 	}
