c35e20d4936cca1cd93a3f30db77900d9e3394a9
al-ce <23170004+al-ce@users.noreply.github.com>
2025-08-16 19:51:59 -0500

    test(repo/pick): can get row by id

---
+++ tests/repo_pick_test.go
@@ -0,0 +1,27 @@
+package tests
+
+import (
+	"testing"
+
+	"gopgrest/service"
+	"gopgrest/types"
+)
+
+func Test_RepoGetRowById(t *testing.T) {
+	repo := NewTestRepo(t)
+	expAuthors := []types.RowData{AnneCarson, AnneBrontë, VirginiaWoolf}
+
+	for index, auth := range expAuthors {
+		id := index + 1
+		rows, err := repo.GetRowByID("authors", int64(id))
+		if err != nil {
+			t.Fatalf("Could not pick author id %d: %s", id, err)
+		}
+		defer rows.Close()
+		gotRows, err := service.ScanRows(rows)
+		if err != nil {
+			t.Fatalf("Could not scan author id %d: %s", id, err)
+		}
+		checkMapEquality(t, []types.RowData{auth}, gotRows)
+	}
+}
