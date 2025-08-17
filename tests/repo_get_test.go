d220467de3eef9dc30e1d524ed786697e37b10aa
al-ce <23170004+al-ce@users.noreply.github.com>
2025-08-17 12:02:10 -0500

    chore(test): condense Get tests into one file

--- tests/repo_get_test.go
+++ tests/repo_get_test.go
@@ -8,6 +8,27 @@ import (
 	"gopgrest/types"
 )
 
+func Test_RepoGetRowById(t *testing.T) {
+	repo := NewTestRepo(t)
+	expAuthors := []types.RowData{AnneCarson, AnneBrontë, VirginiaWoolf}
+
+	for index, auth := range expAuthors {
+		id := index + 1
+		rows, err := repo.GetRowByID("authors", int64(id))
+		if err != nil {
+			t.Fatalf("Could not pick author id %d: %s", id, err)
+		}
+		defer rows.Close()
+		gotRows, err := service.ScanRows(rows)
+		if err != nil {
+			t.Fatalf("Could not scan author id %d: %s", id, err)
+		}
+		if err := checkMapEquality([]types.RowData{auth}, gotRows); err != nil {
+			t.Error(err)
+		}
+	}
+}
+
 func Test_RepoGetRows_NoQuery(t *testing.T) {
 	// GET /authors
 	t.Run("No RSQL query", func(t *testing.T) {
