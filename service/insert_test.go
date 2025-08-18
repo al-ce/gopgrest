663ef7cef89b9e0354f100c889ca1abfcbcf0be2
al-ce <23170004+al-ce@users.noreply.github.com>
2025-08-17 19:58:28 -0500

    test(service/insert): can insert multiple rows

---
+++ service/insert_test.go
@@ -0,0 +1,59 @@
+package service_test
+
+import (
+	"fmt"
+	"strings"
+	"testing"
+
+	"gopgrest/tests"
+	"gopgrest/types"
+)
+
+func Test_ServiceInsertRow_Single(t *testing.T) {
+	service := tests.NewTestService(t)
+
+	newRows := []types.RowData{
+		{
+			"forename": "N.K.",
+			"surname":  "Jemisin",
+			"born":     int64(1972),
+			"died":     nil,
+		},
+		{
+			"forename": "Martha",
+			"surname":  "Nussbaum",
+			"born":     int64(1947),
+			"died":     nil,
+		},
+	}
+
+	ids, err := service.InsertRows(newRows, "authors")
+	if err != nil {
+		t.Fatalf("Could not insert authors\n%v:\n%s", newRows, err)
+	}
+
+	// Turn got ids into str to retrieve from db in one query
+	idStrs := make([]string, len(ids))
+	for i, id := range ids {
+		idStrs[i] = fmt.Sprintf("%d", id)
+	}
+	gotRows, err := tests.SelectRows(
+		service.Repo,
+		fmt.Sprintf(
+			"SELECT * FROM authors WHERE id IN (%s) ORDER BY id",
+			strings.Join(idStrs, ","),
+		),
+	)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	for idx, expAuthor := range newRows {
+		gotAuthor := gotRows[idx]
+		for k, v := range expAuthor {
+			if v != gotAuthor[k] {
+				t.Errorf("Exp %s %v %T:\nGot %v %T", k, v, v, gotAuthor[k], gotAuthor[k])
+			}
+		}
+	}
+}
